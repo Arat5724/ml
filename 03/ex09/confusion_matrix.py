e71ec9b336071e3c75ba00b028d7131183b9b167
Arat5724 <arat5724@gmail.com>
2023-05-05 20:13:41 +0900

    feat: 03/ex09

--- 03/ex09/confusion_matrix.py
+++ 03/ex09/confusion_matrix.py
@@ -1,12 +1,23 @@
 import numpy as np
 from numpy import ndarray
-import pandas as pd
-from pandas import DataFrame, Series
-
-import time
+from pandas import DataFrame
 
 
-def confusion_matrix_(y_true: ndarray, y_hat: ndarray, labels=None, df_option=False):
+def typechecker(fun):
+    def wrapper(y_true, y_hat, labels=None, df_option=False):
+        try:
+            if (isinstance(y_true, ndarray) and isinstance(y_hat, ndarray) and
+                isinstance(labels, (list, type(None))) and isinstance(df_option, bool) and
+                    y_true.shape == y_hat.shape):
+                return fun(y_true, y_hat, labels, df_option)
+        except Exception as e:
+            print(e)
+    return wrapper
+
+
+@typechecker
+def confusion_matrix_(y_true: ndarray, y_hat: ndarray,
+                      labels: list | None = None, df_option: bool = False) -> ndarray | DataFrame | None:
     """
     Compute confusion matrix to evaluate the accuracy of a classification.
     Args:
@@ -26,8 +37,35 @@ def confusion_matrix_(y_true: ndarray, y_hat: ndarray, labels=None, df_option=Fa
     if labels is None:
         labels = list(set(y_true.tolist() + y_hat.tolist()))
         labels.sort()
-    t = np.stack([y_true == t for t in labels], dtype=int)
-    p = np.stack([y_hat == p for p in labels], dtype=int)
+    true = np.vstack([y_true == label for label in labels], dtype=int)
+    prediction = np.vstack([y_hat == label for label in labels], dtype=int)
     if df_option:
-        return DataFrame(data=np.dot(t, p.T), index=labels, columns=labels)
-    return np.dot(t, p.T)
+        return DataFrame(data=np.dot(true, prediction.T), index=labels, columns=labels)
+    return np.dot(true, prediction.T)
+
+
+if __name__ == "__main__":
+    from sklearn.metrics import confusion_matrix
+
+    y_hat = np.array([['norminet'], ['dog'], ['norminet'],
+                     ['norminet'], ['dog'], ['bird']])
+    y = np.array([['dog'], ['dog'], ['norminet'], [
+                 'norminet'], ['dog'], ['norminet']])
+
+    # Example 1:
+    # your implementation
+    print(confusion_matrix_(y, y_hat))
+    # sklearn implementation
+    print(confusion_matrix(y, y_hat))
+
+    # Example 2:
+    # your implementation
+    print(confusion_matrix_(y, y_hat, labels=['dog', 'norminet']))
+    # sklearn implementation
+    print(confusion_matrix(y, y_hat, labels=['dog', 'norminet']))
+
+    # Example 3:
+    print(confusion_matrix_(y, y_hat, df_option=True))
+
+    # Example 4:
+    print(confusion_matrix_(y, y_hat, labels=['bird', 'dog'], df_option=True))
